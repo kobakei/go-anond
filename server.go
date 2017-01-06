a6d39ac4524bd516fd1805fd0d799f3edfaa54fe
Keisuke Kobayashi <kkobayashi@coubic.com>
2017-01-06 19:09:22 +0900

    Add view template

--- server.go
+++ server.go
@@ -1,32 +1,52 @@
 package main
 
 import (
-	"net/http"
 	"github.com/labstack/echo"
+	"html/template"
+	"io"
+	"net/http"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func GetArticles(c echo.Context) error {
-  return c.String(http.StatusOK, "Index page")
+	return c.Render(http.StatusOK, "articles/index", "World")
 }
 
 func NewArticle(c echo.Context) error {
-  return c.String(http.StatusOK, "New page")
+	return c.Render(http.StatusOK, "articles/new", "World")
 }
 
 func GetArticle(c echo.Context) error {
-  return c.String(http.StatusOK, "Get page")
+	return c.Render(http.StatusOK, "articles/show", "World")
 }
 
 func SaveArticle(c echo.Context) error {
-  return c.String(http.StatusOK, "Save page")
+	return c.Render(http.StatusOK, "articles/create", "World")
 }
 
 func main() {
 	e := echo.New()
+
+	// テンプレートの設定
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/**/*.html")),
+	}
+	e.Renderer = t
+
+	// ルーティング
 	e.GET("/", GetArticles)
-  e.GET("/articles", GetArticles)
-  e.GET("/articles/new", NewArticle)
-  e.GET("/articles/:id", GetArticle)
-  e.POST("/articles", SaveArticle)
+	e.GET("/articles", GetArticles)
+	e.GET("/articles/new", NewArticle)
+	e.GET("/articles/:id", GetArticle)
+	e.POST("/articles", SaveArticle)
+
+	// 起動
 	e.Logger.Fatal(e.Start(":1323"))
 }

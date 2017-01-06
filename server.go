8f1f4f31a7f4b41327c28b8a828c25996497e422
Keisuke Kobayashi <kkobayashi@coubic.com>
2017-01-06 19:02:32 +0900

    Add routing

--- server.go
+++ server.go
@@ -5,10 +5,28 @@ import (
 	"github.com/labstack/echo"
 )
 
+func GetArticles(c echo.Context) error {
+  return c.String(http.StatusOK, "Index page")
+}
+
+func NewArticle(c echo.Context) error {
+  return c.String(http.StatusOK, "New page")
+}
+
+func GetArticle(c echo.Context) error {
+  return c.String(http.StatusOK, "Get page")
+}
+
+func SaveArticle(c echo.Context) error {
+  return c.String(http.StatusOK, "Save page")
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", GetArticles)
+  e.GET("/articles", GetArticles)
+  e.GET("/articles/new", NewArticle)
+  e.GET("/articles/:id", GetArticle)
+  e.POST("/articles", SaveArticle)
 	e.Logger.Fatal(e.Start(":1323"))
 }
